4f7c36868ef26de7fb3463a78aef5530a389e1aa
Alberto Osio <osioalberto@gmail.com>
2021-01-29 09:39:08 +0100

    Admin users can now pull and push everything

--- backend/main/scope.py
+++ backend/main/scope.py
@@ -71,6 +71,7 @@ def assert_scope(scope, user):
     if user.is_staff:
         actions_avaiable.add("*")
         actions_avaiable.add("pull")
+        actions_avaiable.add("push")
     for entry in (
         ACLEntry.objects.filter(Q(user=user) & Q(type=scope["type"]))
         .annotate(matches=RawSQL("%s GLOB name", (scope["name"],)))
